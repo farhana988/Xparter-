d168a442f410f38a1929bc7454e849aa84686bb9
farhana988 <farhana744255@gmail.com>
2025-07-14 15:03:19 +0600

    mockup section

--- src/Components/Home/Mockup.jsx
+++ src/Components/Home/Mockup.jsx
@@ -1,4 +1,5 @@
 import FreePic from "../reusable/FreePic";
+import mock from "../../assets/images/mockup.jpg";
 
 const cardData = [
   {
@@ -23,19 +24,29 @@ const cardData = [
 
 const Mockup = () => {
   return (
-    <div className="grid grid-cols-2 py-10 ">
-      {/* image */}
-      <section></section>
-      {/* card */}
-      <section className="flex flex-col items-end mr-20 gap-6 border">
+    <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto
+    max-w-[425px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[1440px]">
+      {/* background image section */}
+      <section
+        className="bg-cover lg:bg-center col-span-1 lg:col-span-2
+        h-60 md:h-[450px] lg:h-full"
+        style={{ backgroundImage: `url(${mock})` }}
+      ></section>
+
+      {/* card section */}
+      <section className="flex flex-col md:flex-row lg:flex-col items-center my-9 
+      col-span-1 gap-8 lg:gap-0">
         {cardData.map(({ id, title, description }) => (
-          <div
-            key={id}
-            className="border px-4 pt-4 pb-14 shadow-md bg-white w-2/3 right-auto"
-          >
-            <h3 className="text-2xl font-bold mb-6">{title}</h3>
-            <p className="text-sm text-gray-600 font-medium">{description}</p>
-            {id === 1 && <FreePic className="mt-4" />}
+          <div key={id} className="w-10/12 h-full mb-2 xl:mb-4">
+            <div className="bg-white px-6 pt-4 pb-7 xl:pb-14  h-full">
+              <h3 className="text-lg xl:text-2xl font-bold mb-3 xl:mb-6">
+                {title}
+              </h3>
+              <p className="text-xs xl:text-sm text-gray-600 font-medium">
+                {description}
+              </p>
+              {id === 1 && <FreePic className="mt-4" />}
+            </div>
           </div>
         ))}
       </section>
